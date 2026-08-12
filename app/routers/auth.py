deb53934a58a0cc67947094af40ffc399de325ff
Jakub D <114111975+JakubD02@users.noreply.github.com>
2026-08-12 13:39:37 +0200

    feat(auth): add user registration endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,9 +5,11 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette import status
 
-from app.core.security import create_access_token
+from app.core.security import create_access_token, create_refresh_token
 from app.database import get_db
-from app.services.auth_service import authenticate_user
+from app.schemas.token import TokenPair
+from app.schemas.user import UserCreate, UserRead
+from app.services.auth_service import authenticate_user, create_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -15,7 +17,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 form_data_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 
-@router.post("/token")
+@router.post("/token", response_model=TokenPair)
 async def login_for_access_token(db: db_dependency, form_data: form_data_dependency):
     user = authenticate_user(
         db=db, username=form_data.username, password=form_data.password
@@ -25,6 +27,18 @@ async def login_for_access_token(db: db_dependency, form_data: form_data_depende
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong login or password!"
         )
 
-    access_token = create_access_token(username=user.username, user_id=user.id)
+    return TokenPair(
+        access_token=create_access_token(email=user.email, user_id=user.id),
+        refresh_token=create_refresh_token(email=user.email, user_id=user.id),
+    )
 
-    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserRead)
+async def regster(user_in: UserCreate, db: db_dependency):
+    user = create_user(db=db, user_in=user_in)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered",
+        )
+    return user
